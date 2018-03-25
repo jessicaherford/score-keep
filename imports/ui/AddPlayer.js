153c5393436ca8b0fa782848346972a9c5d4b60e
JessicaHerford <jrherford12@gmail.com>
2018-03-25 15:30:34 -0600

    adding styles with sass

--- imports/ui/AddPlayer.js
+++ imports/ui/AddPlayer.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
+import {Players} from './../api/players';
+
+const handleSubmit = (e) => {
+
+};
+
 export default class AddPlayer extends React.Component {
+  handleSubmit(e){
+    let playerName = e.target.playerName.value;
+
+    e.preventDefault();
+
+    if (playerName){
+      e.target.playerName.value = '';
+      Players.insert({
+        name: playerName,
+        score: 0
+      });
+    }
+  }
   render() {
     return (
-      <div>
-        <p>This is where player will Render. </p>
+      <div className='item'>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input type="text" name="playerName" placeholder="Player name"/>
+          <button className="button">Add Player</button>
+        </form>
       </div>
     );
   }
--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class TitleBar extends React.Component {
+export default class TitleBar extends React.Component{
+  renderSubtitle() {
+    if (this.props.subtitle){
+      return <h3>{this.props.subtitle}</h3>
+    }
+  }
   render() {
     return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h3>{this.props.subtitle}</h3>
+      <div className="title-bar">
+        <div className="wrapper">
+          <h1>{this.props.title}</h1>
+          {this.renderSubtitle()}
+        </div>
       </div>
     );
   }
@@ -14,7 +21,7 @@ export default class TitleBar extends React.Component {
 
 TitleBar.propTypes = {
   title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired
+  subtitle: PropTypes.string
 };
 
 //TitleBar.defaultProps = {
